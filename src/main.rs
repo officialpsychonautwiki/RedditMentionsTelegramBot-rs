d8006f6b9299737551f9ba0c2ed47d0b027c7dda
Kenan Sulayman <kenan@sly.mn>
2016-12-08 03:07:11 +0100

    Add permalinks to telegram messages

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ extern crate json;
 use hyper::Url;
 use regex::Regex;
 
+use json::JsonValue::Null;
+
 fn main() {
     let re = [
         Regex::new(r"psychonaut\s?wiki").unwrap(),
         Regex::new(r"psychonaut.?wiki").unwrap(),
         Regex::new(r"disregard\s?everything\s?i\s?say").unwrap()
     ];
+
+    let reddit_rgx = Regex::new(r"www.reddit.com").unwrap();
 
     let api = telegram_bot::Api::from_env("TELEGRAM_TOKEN").unwrap();
 
@@ -22,10 +26,32 @@ fn main() {
     for event in client {
         let uw_event = event.unwrap();
 
-        if re.iter().any(|ref exp| exp.is_match(&uw_event.data)) {
-            let evt = json::parse(&uw_event.data).unwrap();
+        let evt = json::parse(&uw_event.data).unwrap();
 
-            let msg = format!("User {:?} wrote into subreddit {:?} with title {:?}:\n\n{:?}", evt["author"].to_string(), evt["subreddit_id"].to_string(), evt["link_title"].to_string(), evt["body"].to_string());
+        if re.iter().any(|ref exp| exp.is_match(&uw_event.data)) {
+            let link = {
+                let linkstr = evt["link_url"].to_string();
+
+                let link_id = evt["link_id"].to_string();
+                let mut r_linkid = link_id.chars();
+                r_linkid.by_ref().nth(2);
+                let link_id = r_linkid.as_str();
+
+                if !reddit_rgx.is_match(&linkstr) {
+                    format!("https://www.reddit.com/r/{}/comments/{}/_/{}/", evt["subreddit"], link_id, evt["id"])
+                } else {
+                    format!("{}{}", evt["link_url"].to_string(), evt["link_id"].to_string())
+                }
+            };
+
+            let msg = format!(
+                "User {:?} wrote into subreddit {:?} with title {:?}:\n\n» {} «\n\nPermalink: {}",
+                evt["author"].to_string(),
+                evt["subreddit"].to_string(),
+                evt["link_title"].to_string(),
+                evt["body"].to_string(),
+                link.to_string()
+            );
 
             let _ = api.send_message(
                 -1001084499328 as i64,
